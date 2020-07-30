2e3b1e78c347599cc3e13d4024b3a2a1d0ae77c7
sjbothwell <46718943+sjbothwell@users.noreply.github.com>
2020-07-30 12:32:37 -0600

    tidy

--- R/box_labels.R
+++ R/box_labels.R
@@ -1,27 +1,16 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+# Hello, world!  This is an example function named 'hello' which prints 'Hello, world!'.  You can learn more about package authoring with RStudio at:
+# http://r-pkgs.had.co.nz/ Some useful keyboard shortcuts for package authoring: Install Package: 'Ctrl + Shift + B' Check Package: 'Ctrl + Shift + E' Test Package:
+# 'Ctrl + Shift + T'
 
 box.lbls <- function(x, color = NULL) {
-  boxplot(x, col = color)
-  text(x = 0.65, y = median(x), labels = paste("Median = ", median(x)))
-  text(x = 0.65, y = quantile(x, 0.25), labels = paste("Q1 = ", quantile(x, 0.25)))
-  text(x = 0.65, y = quantile(x, 0.75), labels = paste("Q3 = ", quantile(x, 0.75)))
-  text(x = 0.65, y = min(x), labels = paste("Min = ", min(x)))
-  text(x = 0.65, y = max(x), labels = paste("Max = ", max(x)))
+    boxplot(x, col = color)
+    text(x = 0.65, y = median(x), labels = paste("Median = ", median(x)))
+    text(x = 0.65, y = quantile(x, 0.25), labels = paste("Q1 = ", quantile(x, 0.25)))
+    text(x = 0.65, y = quantile(x, 0.75), labels = paste("Q3 = ", quantile(x, 0.75)))
+    text(x = 0.65, y = min(x), labels = paste("Min = ", min(x)))
+    text(x = 0.65, y = max(x), labels = paste("Max = ", max(x)))
 }
 
-x <- c(1,6,2,9,18,12,6,3,10,21,2,3,7,1)
+x <- c(1, 6, 2, 9, 18, 12, 6, 3, 10, 21, 2, 3, 7, 1)
 
 box.lbls(x, color = "lightblue")
