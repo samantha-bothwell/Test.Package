0c426bea8bdacf5e02e20c1c3db74c1a246981d4
sjbothwell <46718943+sjbothwell@users.noreply.github.com>
2020-08-03 09:01:45 -0600

    Add ggplot function

--- R/ggbox_lbls.R
+++ R/ggbox_lbls.R
@@ -3,15 +3,6 @@
 #' This function labels summary statistics on a ggplot boxplot. The summary statistics reported
 #' are the minimum, maximum, Q1, Q3 and median. To use the function, you must specify a vector \code{x}.
 #'
-#' Function used in the registration step of an FPCA-based approach for
-#' registering exponential-family functional data, called by \code{\link{register_fpca}}.
-#' This method uses constrained optimization to estimate spline
-#' coefficients for warping functions, where the objective function for optimization comes from
-#' maximizing the EF likelihood subject to monotonicity constraints on the warping functions.
-#' You have to either specify \code{obj}, which is a fpca
-#' object from an earlier step, or \code{Y}, a dataframe in long format with variables
-#' \code{id}, \code{index}, and \code{value} to indicate subject IDs, times, and observations,
-#' respectively.
 #'
 #' @param x for specifying data from which the boxplots are to be produced. Either a numeric vector, or a single list containing such vectors.
 #' @param color the color of the body of the boxplot. By default, the boxplot color is the same as the background color.
@@ -23,8 +14,21 @@
 #' @importFrom ggplot2
 #'
 #' @examples
-#' box_lbls(rnorm(1000), color = "pink")
+#' ggbox_lbls(rnorm(1000), color = "pink")
 #'
 #' x <- c(1,6,3,9,7,5,9,16,4)
-#' box_lbls(x, color = "lightblue")
+#' ggbox_lbls(x, color = "steelblue1")
 #'
+
+ggbox_lbls <- function(x, color = NULL) {
+  x <- as.data.frame(x)
+  ggplot(data = x, aes(x = "", y = x)) +
+    geom_boxplot(fill = color) +
+    annotate("text", x = 0.8, y = median(x[,1]) + 0.5, label =  paste("Median = ", median(x[,1]))) +
+    annotate("text", x = 0.8, y = quantile(x[,1], 0.25) + 0.5, label =  paste("Q1 = ", quantile(x[,1], 0.25))) +
+    annotate("text", x = 0.8, y = quantile(x[,1], 0.75) + 0.5, label =  paste("Q3 = ", quantile(x[,1], 0.75))) +
+    annotate("text", x = 0.8, y = min(x[,1]), label =  paste("Min = ", min(x[,1]))) +
+    annotate("text", x = 0.8, y = max(x[,1]), label =  paste("Max = ", max(x[,1])))
+}
+
+
