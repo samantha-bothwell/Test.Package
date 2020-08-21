412b0960b8396cec9135cae65d73fc6a4b8fe9cc
sjbothwell <46718943+sjbothwell@users.noreply.github.com>
2020-08-21 10:17:06 -0600

    Test class samantha

--- tests/testthat/test_ggbox_lbls.R
+++ tests/testthat/test_ggbox_lbls.R
@@ -2,8 +2,15 @@ library(testthat)
 library(test.package)
 
 context("ggbox_lbls")
-test_that("ggbox_lbls only takes numeric vector values", {
+test_that("ggbox_lbls only takes class numeric or class samantha", {
   x <- rnorm(100, 0, 1)
-  g <- ggbox_lbls(x, color = "lightblue")
+  g <- ggbox_lbls(x)
+  expect_equal(class(g)[1], "gg")
+})
+
+test_that("ggbox_lbls only takes class numeric or class samantha", {
+  x <- rnorm(1000)
+  class(x) <- "samantha"
+  g <- ggbox_lbls(x)
   expect_equal(class(g)[1], "gg")
 })
